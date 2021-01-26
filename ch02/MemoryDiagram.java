66f51752b3b981bf0e826f12ddfb3d4a82a59376
Edgar Rosales <edgarr15@gmail.com>
2021-01-26 13:10:57 -0800

    Added a print statement to output the variable values

--- ch02/MemoryDiagram.java
+++ ch02/MemoryDiagram.java
@@ -5,6 +5,8 @@ public class MemoryDiagram {
         int b = a;     // a and b are now equal
         a = 3;         // a and b are no longer equal
         int c = 0;
+        //print the updated values of the variables a, b and c
+        System.out.println("The value of a is: " + a + "\nThe value of b is: " + b + "\nThe value of c is: " + c);
     }
 
 }
