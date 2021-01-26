e67efa72c19a67404c16bdb7f37b6ff7107beb6a
Edgar Rosales <edgarr15@gmail.com>
2021-01-26 10:54:19 -0800

    Added comment on main method

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        // generate some simple output
+        // generate some simple text output to greet user
         System.out.println("Hello, World!");
     }
 
