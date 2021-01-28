3e97c1d022decf990be8dd2a645f5bed6ea2e458
Edgar Rosales <edgarr15@gmail.com>
2021-01-27 18:26:38 -0800

    Added a method to print a statement 3 and four times

--- ch04/PrintTwice.java
+++ ch04/PrintTwice.java
@@ -5,11 +5,30 @@ public class PrintTwice {
         System.out.println(s);
     }
 
+    public static void printThrice(String s) {
+    	System.out.println(s);
+    	System.out.println(s);
+    	System.out.println(s);
+    	
+    }
+    
+    public static void printTwiceTwice(String s) {
+    	printTwice(s);
+    	printTwice(s);
+    }
     public static void main(String[] args) {
         printTwice("Don't make me say this twice!");
 
         String message = "Never say never.";
         printTwice(message);
+        
+        String message2 = "There is no time like today";
+        printThrice(message2);
+    
+        String message3 = "Does that makes sense to you?";
+        printTwiceTwice(message3);
     }
 
+    
+    
 }
